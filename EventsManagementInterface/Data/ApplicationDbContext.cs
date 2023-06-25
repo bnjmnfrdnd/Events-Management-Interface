52b988a507c1858896e0bc1539414d1ed81a83b9
Benjamin Ferdinand <benjaminferdinand@outlook.com>
2023-06-25 21:26:00 +0100

    Add project files.

---
+++ EventsManagementInterface/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsManagementInterface.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
