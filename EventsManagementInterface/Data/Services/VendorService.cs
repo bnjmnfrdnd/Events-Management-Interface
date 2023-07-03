1a7cc0d000f923dba5b404bde48f1b16b65ae635
bferdinandcl <benjamin.ferdinand@clarivate.com>
2023-07-03 18:53:42 +0100

    - Added vendor and change log page to the navigation menu  - Updated the colour scheme to be more user-friendly and less distracting  - Vendor input now has validation to prevent submitting data with:         No GIN number         Incorrect GIN number         No allowance token value provided         GIN number with an exceeded allowance limit

--- EventsManagementInterface/Data/Services/VendorService.cs
+++ EventsManagementInterface/Data/Services/VendorService.cs
@@ -15,54 +15,144 @@ namespace EventsManagementInterface.Data.Services
             this.logService = logService;
         }
 
-
         public async Task<VendorInputModal> SubmitVendorInput(VendorInput vendorInput)
         {
-            Attendee attendee = database.Attendee.SingleOrDefault(x => x.GuestIdentificationNumber == vendorInput.GuestIdentificationNumber);
-            VendorInputModal vendorInputModal = new VendorInputModal();
+            try
+            {
+                bool allowanceExceeded = false;
+                List<string> errors = new List<string>();
+                VendorInputModal vendorInputModal = new VendorInputModal();
 
-            if (attendee == null) {
+                #region If no vendor input
 
-                vendorInputModal = new VendorInputModal
+                if (vendorInput.AlcoholicDrinkToken == 0 && vendorInput.NonAlcoholicDrinkToken == 0 && vendorInput.FoodToken == 0)
                 {
-                    GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
-                    GuestName = "",
-                    Success = false,
-                    Errors = new List<string> {
+                    vendorInputModal = new VendorInputModal
+                    {
+                        GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
+                        GuestName = $"",
+                        Success = false,
+                        Errors = new List<string>(),
+                        TokensRemaining = new List<string>(),
+                        Message = $"Please provide a token value for either, food, alcoholic drinks or non-alcoholic drinks.",
+                        Title = "Error"
+                    };
+
+                    return vendorInputModal;
+                }
+
+                #endregion
+
+                Attendee attendee = database.Attendee.SingleOrDefault(x => x.GuestIdentificationNumber == vendorInput.GuestIdentificationNumber);
+
+                if (attendee == null)
+                {
+
+                    vendorInputModal = new VendorInputModal
+                    {
+                        GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
+                        GuestName = "",
+                        Success = false,
+                        Errors = new List<string> {
                         $"Guest Identificiation Number ({vendorInput.GuestIdentificationNumber}) does not exist"
                     },
-                    Message = "Please try again",
-                    Title = "Error",
-                    TokensRemaining = new List<string>()
-                };
+                        Message = "Please try again",
+                        Title = "Error",
+                        TokensRemaining = new List<string>()
+                    };
 
-                return vendorInputModal;
-            }
+                    return vendorInputModal;
+                }
 
-            attendee.AlcoholicDrinkTokenAllowance -= vendorInput.AlcoholicDrinkToken;
-            attendee.NonAlcoholicDrinkTokenAllowance -= vendorInput.NonAlcoholicDrinkToken;
-            attendee.FoodTokenAllowance -= vendorInput.FoodToken;
+                #region If allowance has been exceeded
 
-            vendorInputModal = new VendorInputModal
-            {
-                GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
-                GuestName = $"{attendee.FirstName} {attendee.LastName}",
-                Success = true,
-                Errors = new List<string>(),
-                TokensRemaining = new List<string>
+                if (vendorInput.AlcoholicDrinkToken > 0 && attendee.AlcoholicDrinkTokenAllowance <= 0)
+                {
+                    allowanceExceeded = true;
+                    errors.Add($"There are no alcoholic drink tokens remaining");
+                }
+
+                if (vendorInput.NonAlcoholicDrinkToken > 0 && attendee.NonAlcoholicDrinkTokenAllowance <= 0)
+                {
+                    allowanceExceeded = true;
+                    errors.Add("There are no non-alcoholic drink tokens remaining");
+                }
+
+                if (vendorInput.FoodToken > 0 && attendee.FoodTokenAllowance <= 0)
+                {
+                    allowanceExceeded = true;
+                    errors.Add("There are no food drink tokens remaining");
+                }
+
+                if (allowanceExceeded)
+                {
+                    vendorInputModal = new VendorInputModal
+                    {
+                        GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
+                        GuestName = $"{attendee.FirstName} {attendee.LastName}",
+                        Success = false,
+                        Errors = errors,
+                        TokensRemaining = new List<string>
                 {
                     $"Alcohol tokens: {attendee.AlcoholicDrinkTokenAllowance}",
                     $"Non-Alcohol tokens: {attendee.NonAlcoholicDrinkTokenAllowance}",
                     $"Food tokens: {attendee.FoodTokenAllowance}"
                 },
-                Message = $"Tokens have been deducted for {attendee.FirstName} {attendee.LastName}",
-                Title = "Success"
-            };
+                        Message = $"There has been a problem for {attendee.FirstName} {attendee.LastName}",
+                        Title = "Error"
+                    };
 
-            database.Update(attendee);
-            database.SaveChanges();
+                    return vendorInputModal;
+                }
 
-            return vendorInputModal;
+                #endregion
+
+                attendee.AlcoholicDrinkTokenAllowance -= vendorInput.AlcoholicDrinkToken;
+                attendee.NonAlcoholicDrinkTokenAllowance -= vendorInput.NonAlcoholicDrinkToken;
+                attendee.FoodTokenAllowance -= vendorInput.FoodToken;
+
+                vendorInputModal = new VendorInputModal
+                {
+                    GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
+                    GuestName = $"{attendee.FirstName} {attendee.LastName}",
+                    Success = true,
+                    Errors = new List<string>(),
+                    TokensRemaining = new List<string>
+                {
+                    $"Alcohol tokens: {attendee.AlcoholicDrinkTokenAllowance}",
+                    $"Non-Alcohol tokens: {attendee.NonAlcoholicDrinkTokenAllowance}",
+                    $"Food tokens: {attendee.FoodTokenAllowance}"
+                },
+                    Message = $"Tokens have been deducted for {attendee.FirstName} {attendee.LastName}",
+                    Title = "Success"
+                };
+
+                database.Update(attendee);
+                database.SaveChanges();
+
+                return vendorInputModal;
+
+            }
+            catch (Exception ex)
+            {
+                VendorInputModal vendorInputModal = new VendorInputModal
+                {
+                    GuestIdentificationNumber = vendorInput.GuestIdentificationNumber,
+                    GuestName = String.Empty,
+                    Success = true,
+                    Errors = new List<string>
+                    {
+                        $"Message: {ex.Message}",
+                        $"Inner exception: {(ex.InnerException != null ? ex.InnerException.ToString() : "")}",
+                        $"Stacktrace: {(ex.StackTrace != null ? ex.StackTrace.ToString() : "")}"
+                    },
+                    TokensRemaining = new List<string>(),
+                    Message = $"The system administrator has been informed. Please try again.",
+                    Title = "Error"
+                };
+
+                return vendorInputModal;
+            }
         }
     }
 } 
