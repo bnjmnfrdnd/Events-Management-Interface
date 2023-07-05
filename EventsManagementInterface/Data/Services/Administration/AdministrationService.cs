e3b7c2e65859bc5cab2801e8265486c9c22a39f3
bferdinandcl <benjamin.ferdinand@clarivate.com>
2023-07-05 19:54:21 +0100

    1.0.4

--- EventsManagementInterface/Data/Services/Administration/AdministrationService.cs
+++ EventsManagementInterface/Data/Services/Administration/AdministrationService.cs
@@ -1,6 +1,7 @@
 ﻿using EventsManagementInterface.Data.Models;
 using EventsManagementInterface.Data.Models.Administration;
 using EventsManagementInterface.Data.Models.Attendee;
+using EventsManagementInterface.Data.Models.Vendor;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventsManagementInterface.Data.Services
@@ -9,10 +10,12 @@ namespace EventsManagementInterface.Data.Services
     {
 
         private ApplicationDbContext database;
+        private Utility utility;
 
-        public AdministrationService(ApplicationDbContext database)
+        public AdministrationService(ApplicationDbContext database, Utility utility)
         {
             this.database = database;
+            this.utility = utility;
         }
 
         public async Task<List<Attendee>> GetAttendees()
@@ -31,7 +34,7 @@ namespace EventsManagementInterface.Data.Services
                 {
                     Success = true,
                     Title = "Success",
-                    Message = "Please see token allowance below",
+                    Message = $"Please see token allowance below. An email has also been sent to {attendee.EmailAddress}.",
                     TokensRemaining = new List<string>
                     {
                         $"Alcoholic drink tokens: {attendee.AlcoholicDrinkTokenAllowance}",
@@ -40,8 +43,22 @@ namespace EventsManagementInterface.Data.Services
                     }
                 };
 
-                database.Update(attendee);
-                database.SaveChanges();
+                Utility.SendEmail(new Models.Email.Email
+                {
+                    Recipient = attendee.EmailAddress,
+                    Subject = "Coloplast Fund Day Allowance",
+                    HTMLMessage = 
+                    $"Hi {attendee.FirstName}, " +
+                    $"<br/><br/> " +
+                    $"Your remaining token allowance is: <ul>" +
+                    $"<li>Alcoholic Drink Tokens: {attendee.AlcoholicDrinkTokenAllowance}</li>" +
+                    $"<li>Non-Alcoholic Drink Tokens: {attendee.NonAlcoholicDrinkTokenAllowance}</li>" +
+                    $"<li>Food Tokens: {attendee.FoodTokenAllowance}</li>" +
+                    $"</ul>" +
+                    $"<br/>" +
+                    $"Kind regards, <br>" +
+                    $"Coloplast Fun Day Team"
+                });
 
                 return baseModal;
             }
@@ -51,7 +68,11 @@ namespace EventsManagementInterface.Data.Services
                 {
                     Success = false,
                     Title = "Error",
-                    Message = "The GIN number has not been found",
+                    Message = "There was a problem with this request, please see the errors below",
+                    Errors = new List<string>()
+                    {
+                        $"Guest Identificiation Number ({guestManagement.GuestIdentificationNumber}) does not exist"
+                    }
                 };
 
                 return baseModal;
@@ -73,11 +94,28 @@ namespace EventsManagementInterface.Data.Services
                 {
                     Success = true,
                     Title = "Success",
-                    Message = "Token allowance has been updated",
+                    Message = $"Token allowance has been updated. An email has been sent to {attendee.EmailAddress}.",
                 };
 
                 database.Update(attendee);
                 database.SaveChanges();
+
+                Utility.SendEmail(new Models.Email.Email
+                {
+                    Recipient = attendee.EmailAddress,
+                    Subject = "Coloplast Fund Day Allowance",
+                    HTMLMessage =
+                    $"Hi {attendee.FirstName}, " +
+                    $"<br/><br/> " +
+                    $"Your new token allowance is: <ul>" +
+                    $"<li>Alcoholic Drink Tokens: {attendee.AlcoholicDrinkTokenAllowance}</li>" +
+                    $"<li>Non-Alcoholic Drink Tokens: {attendee.NonAlcoholicDrinkTokenAllowance}</li>" +
+                    $"<li>Food Tokens: {attendee.FoodTokenAllowance}</li>" +
+                    $"</ul>" +
+                    $"<br/>" +
+                    $"Kind regards, <br>" +
+                    $"Coloplast Fun Day Team"
+                });
 
                 return baseModal;
             }
